e286d4662893d85e7a7b6d60ae7c45e0fe08cd02
Jayson Binabese <102567240+jays-github2022@users.noreply.github.com>
2023-03-28 11:17:57 +0800

    Add files via upload

---
+++ Lab6.py
@@ -0,0 +1,20 @@
+# Define a list 
+z = [12, 20, 18, 23]
+print(z)
+
+# Define a list
+heterogenous_elements = [2, True, 'Jayson', 1.5]
+print(heterogenous_elements)
+
+# Access the first item of a list at index 0 
+z [0]
+print(z)
+
+z[-1]
+print(z)
+
+print(z [0:2])
+
+z [:3]
+
+z [1:]
